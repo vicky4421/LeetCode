12d2e5b5b497c1c1b95994846f3fcb6e4d699d4e
Vivek Sawant <vicky210490@gmail.com>
2024-03-13 00:04:35 +0530

    easy ex 1

--- src/easy/ex1/Solution.java
+++ src/easy/ex1/Solution.java
@@ -1,6 +1,19 @@
 package easy.ex1;
 
-public class Solution {
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hashmap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (hashmap.containsKey(diff)) return new int[]{hashmap.get(diff), i};
+            else hashmap.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
     public static void main(String[] args) {
 
     }
