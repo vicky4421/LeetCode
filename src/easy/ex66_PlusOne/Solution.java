479dd8d304059701cdddb170212fd84621d3a956
Vivek Sawant <vicky210490@gmail.com>
2024-03-21 23:04:36 +0530

    easy ex 66: Plus One

---
+++ src/easy/ex66_PlusOne/Solution.java
@@ -0,0 +1,23 @@
+package easy.ex66_PlusOne;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length-1; i >= 0; i--) {
+            if (digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] newArray = new int[digits.length+1];
+        newArray[0] = 1;
+        return newArray;
+    }
+
+    public static void main(String[] args) {
+        int[] digits = {9, 9, 9};
+        System.out.println(Arrays.toString(plusOne(digits)));
+    }
+}
